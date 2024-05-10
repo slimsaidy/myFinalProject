06df72cbc3cdaa0a9b728a4a85a0734a3ad77b64
slimsaidy <142821024+slimsaidy@users.noreply.github.com>
2024-05-09 20:41:21 -0600

    added handle error method

--- src/myfinal/controller/Controller.java
+++ src/myfinal/controller/Controller.java
@@ -1,7 +1,12 @@
 package myfinal.controller;
 
+import javax.swing.JOptionPane;
+import myfinal.view.FinalFrame;
+
 public class Controller 
 {
+	public FinalFrame frame;
+	
 	public Controller()
 	{
 		
@@ -9,6 +14,11 @@ public class Controller
 	
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(frame, "Welcome to my final project!");
+	}
+	
+	public void handleError(Exception error)
+	{
+		JOptionPane.showMessageDialog(frame, error.getMessage(), "final project Error!", JOptionPane.ERROR_MESSAGE);
 	}
 }
